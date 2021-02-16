b32c169ece9ee923de0ea92306d6470a44acf5fc
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:39:37 -0300

    Add files via upload

---
+++ Uri1158/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Uri1158
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, y, soma, cont;
+            soma = 0;
+            cont = 0;
+            n = int.Parse(Console.ReadLine());
+
+
+            for (int c = 0; c < n; c++)
+            {
+
+
+                string[] vetor = Console.ReadLine().Split(' ');
+                x = int.Parse(vetor[0]);
+                y = int.Parse(vetor[1]);
+
+                for (int j = x; cont < y; j++)
+                {
+
+
+                    if (j % 2 != 0)
+                    {
+                        soma = soma + j;
+                        cont = cont + 1;
+                    }
+
+
+                }
+
+
+
+
+
+                Console.WriteLine(soma);
+
+            }
+        }
+    }
+}
